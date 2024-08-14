3fa7876ea96c84a22abe95577b2f52358b15883d
Gurvinder Singh (Garry) <gurvinder604singh@gmail.com>
2024-08-14 16:21:17 +0100

    c1

--- Task1.java
+++ Task1.java
@@ -1,37 +1,50 @@
 import java.util.Random;
 import java.util.Scanner;
 
+//-----------------------------THE NUMBER GAME--------------------------------
+
 public class Task1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+
+        Scanner input = new Scanner(System.in);
+        Random randomNum = new Random();
         int score = 0;
         boolean playAgain = true;
 
-        System.out.println("Welcome to the Guess the Number Game!");
+
+        System.out.println("+--------------------------------------+");
+        System.out.println("+ Welcome to the Guess the Number Game! +");
+        System.out.println("+---------------------------------------+");
+
+
+        //------------------LOOPS AND ATTEMPTS LIMIT-------------------------
 
         while (playAgain) {
-            int lowerBound = 1;
-            int upperBound = 100;
+            int minBound = 1;
+            int maxBound = 100;
             int maxAttempts = 10;
             int attempts = 0;
-            boolean hasWon = false;
+            boolean Won = false;
 
-            // Generate a random number
-            int randomNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+            //--------------GENERATING A RANDOM NUMBER------------------------
 
-            System.out.println("I have generated a random number between " + lowerBound + " and " + upperBound + ".");
+            int randomNumber = randomNum.nextInt(maxBound - minBound + 1) + minBound;
+
+            System.out.println("I have generated a random number between " + minBound + " and " + maxBound + ".");
             System.out.println("You have " + maxAttempts + " attempts to guess the correct number.");
 
-            // Loop for user's guesses
-            while (attempts < maxAttempts && !hasWon) {
+            //----------------LOOP FOR USER GUESS------------------------------
+
+            while (attempts < maxAttempts && !Won) {
                 System.out.print("Enter your guess: ");
-                int userGuess = scanner.nextInt();
+                int userGuess = input.nextInt();
                 attempts++;
 
                 if (userGuess == randomNumber) {
-                    System.out.println("Congratulations! You guessed the correct number.");
-                    hasWon = true;
+                    System.out.println("+--------------------------------------------------+");
+                    System.out.println("+ Congratulations! You guessed the correct number. +");
+                    System.out.println("+--------------------------------------------------+");
+                    Won = true;
                     score += (maxAttempts - attempts + 1); // Increase score based on remaining attempts
                 } else if (userGuess > randomNumber) {
                     System.out.println("Too high! Try again.");
@@ -40,19 +53,20 @@ public class Task1 {
                 }
             }
 
-            if (!hasWon) {
+            if (!Won) {
                 System.out.println("Sorry, you've used all your attempts. The correct number was " + randomNumber + ".");
             }
 
             System.out.println("Your current score is: " + score);
 
-            // Prompt user to play again
+            //--------------------PROMPT USER TO PLAY AGAIN--------------------------
+
             System.out.print("Do you want to play again? (yes/no): ");
-            String userResponse = scanner.next();
-            playAgain = userResponse.equalsIgnoreCase("yes");
+            String userChoice = input.next();
+            playAgain = userChoice.equalsIgnoreCase("yes");
         }
 
         System.out.println("Thanks for playing! Your final score is: " + score);
-        scanner.close();
+        input.close();
     }
 }
