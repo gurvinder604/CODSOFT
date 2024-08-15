71ec71629ab0d9a9bf7e0b622a9ba8712fb3b35e
Gurvinder Singh (Garry) <gurvinder604singh@gmail.com>
2024-08-15 15:54:08 +0100

    c2

--- task2.java
+++ task2.java
@@ -10,56 +10,35 @@ public class task2 {
         int NumOfSubject = input.nextInt();
 
         // Taking marks obtained in each subjects
-        int[] marks = new int[NumOfSubject]; // list of marks, number of elements would be equal to number of subjects
-        int TotalMarks = 0;
+        float[] marks = new float[(int) NumOfSubject]; // list of marks, number of elements would be equal to number of subjects
+        float TotalMarks = 0;
 
         // Storing marks values in list marks[] and calculating Totalmarks
         for (int i=0; i < NumOfSubject; i++) {
             System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
             System.out.print("Please enter the marks for " + (i + 1) + " subject: ");
-            marks[i] = input.nextInt();
+            marks[i] = input.nextFloat();
             TotalMarks += marks[i];
         }
 
         // Calculating Average Percentage
-         double AvgPercent = (double) TotalMarks/NumOfSubject;
+         float AvgPercent = (float) TotalMarks/NumOfSubject;
 
         // simplifying Grade Category
         int GradeCategory = (int) AvgPercent/10;
 
         // Calculating Grades Using switch cases
 
-        String Grade;
-        switch (GradeCategory) {
-            case 10: // this will be skipped and move to case 9 since grade would be same
-            case 9:
-                Grade = "A+";
-                break;
-
-            case 8:
-                Grade = "A";
-                break;
-
-            case 7:
-                Grade = "B";
-                break;
-
-            case 6:
-                Grade = "C";
-                break;
-
-            case 5:
-                Grade = "D";
-                break;
-
-            case 4:
-                Grade = "E";
-                break;
-
-            default:
-                Grade = "F"; // failed
-                break;
-        }
+        String Grade = switch (GradeCategory) {
+            // this will be skipped and move to case 9 since grade would be same
+            case 10, 9 -> "A+";
+            case 8 -> "A";
+            case 7 -> "B";
+            case 6 -> "C";
+            case 5 -> "D";
+            case 4 -> "E";
+            default -> "F"; // failed
+        };
 
         // Display the results
         System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
